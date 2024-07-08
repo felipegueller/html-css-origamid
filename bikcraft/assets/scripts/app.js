9d566e154168d3f3b12ef2d75a7419fc2fdd3d91
Felipe Gueller <felipe.gueller@gmail.com>
2024-07-08 17:19:23 -0300

    :sparkles: feat: add galery logic

--- bikcraft/assets/scripts/app.js
+++ bikcraft/assets/scripts/app.js
@@ -4,7 +4,7 @@ import { checkBudgetInput } from './budget.js'
 
 // Alternar as perguntas frequentes
 const perguntas = document.querySelector('.perguntas-frequentes dl')
-perguntas.addEventListener('click', toggleAccordion)
+perguntas?.addEventListener('click', toggleAccordion)
 
 // Ativar os menus de navegação
 const menuLinks = document.querySelectorAll('nav .header-menu a')
@@ -13,3 +13,23 @@ activeLink(menuLinks)
 // Selecionar o produto para o orçamento
 const urlParams = new URLSearchParams(window.location.search);
 urlParams.forEach(param => checkBudgetInput(param))
+
+const gallery = document.querySelector(".bicicleta-imagens")
+
+gallery?.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    const gallery = event.currentTarget;
+    const invalidOperation = clickedElement.tagName !== "IMG" ||
+        gallery.children[0] === clickedElement ||
+        window.matchMedia("(max-width: 1000px)").matches;
+
+    if (invalidOperation) return;
+
+    const img = document.createElement("img");
+    img.src = clickedElement.src;
+    img.alt = clickedElement.alt;
+    gallery.insertAdjacentElement("afterbegin", img);
+
+    clickedElement.remove();
+})
+
